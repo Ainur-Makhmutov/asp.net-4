043d0e54c6a715fe63adcec0ca599ab8af0e20e2
Ainur-Makhmutov <99477737+Ainur-Makhmutov@users.noreply.github.com>
2023-05-02 21:26:26 +0300

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Лабораторная_работа___4
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Calculator", action = "Index"}
+
+            );
+            routes.MapRoute(
+                name: "Default1",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Calculator", action = "Result" }
+
+            );
+        }
+        protected void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Лабораторная_работа___4.Models;
+
+namespace Лабораторная_работа___4.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result(string Result)
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Calculate(CalculatorModel model, string action)
+        {
+            if (action == "calculate")
+            {
+                if (ModelState.IsValid)
+                {
+                    switch (model.Operation)
+                    {
+                        case "+":
+                            model.Result = model.First_Number + model.Second_Number;
+                            break;
+                        case "-":
+                            model.Result = model.First_Number - model.Second_Number;
+                            break;
+                        case "*":
+                            model.Result = model.First_Number * model.Second_Number;
+                            break;
+                        case "/":
+                            model.Result = model.First_Number / model.Second_Number;
+                            break;
+                    }
+
+                    ViewBag.Message = "Вычисление успешно.";
+                }
+                else
+                {
+                    ViewBag.Message = "Введены неверные данные.";
+                }
+
+                return View("Index", model);
+            }
+            else if (action == "clearr")
+            {
+                model.First_Number = 0;
+                model.Operation = "+";
+
+                ViewBag.Message = "Поля очищены.";
+
+                return View("Index", model);
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+    }
+}
